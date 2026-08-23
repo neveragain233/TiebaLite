7243b3d2861585703cb878ac106c836e32c930ac
neveragain233 <Phalixlu233@gmail.com>
2026-08-23 22:58:17 +0800

    release: 1.2.0 (versionCode 391041)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 391040
-        versionName = "1.1.3"
+        versionCode = 391041
+        versionName = "1.2.0"
         // Configure custom runner to set up the Hilt test application
         testInstrumentationRunner = "$applicationId.TbLiteTestRunner"
         vectorDrawables {
