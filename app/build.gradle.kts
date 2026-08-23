411cc460c55085e30d71075969841ea466466f9f
neveragain233 <Phalixlu233@gmail.com>
2026-08-23 23:17:25 +0800

    release: 1.2.0 (versionCode 391042)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 391041
+        versionCode = 391042
         versionName = "1.2.0"
         // Configure custom runner to set up the Hilt test application
         testInstrumentationRunner = "$applicationId.TbLiteTestRunner"
